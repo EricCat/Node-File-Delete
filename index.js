aaf6e2e9091695f3b227638696b9edb21fa6600b
Liu Wei <sunny5178@gmail.com>
2014-02-08 13:28:23 +0800

    Fixed schedule delete file bug

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ exports.deleteFileSync = function(filePath, timeInterval, callback) {
     function iterator(filePath, dirs, callback) {
         fs.stat(filePath, function(err, stats){
             if(err){
-                if (err.message === 'No such file or directory') {
-                    // Ignore file not found errors and return an empty result
-                    callback(null, "");
-                } else {
-                    // Pass other errors through as is
-                    callback(err);
-                }
+                callback(err);
             } else{
                 if(stats.isDirectory()) {
                     dirs.unshift(filePath);//collection dirs
@@ -32,24 +26,20 @@ exports.deleteFileSync = function(filePath, timeInterval, callback) {
         }
     }
 
-    var ex = function(path, cb){
-        cb = cb || function(){};
+    var interval = timeInterval * 1000;
+    return setInterval(function(){
+        callback = callback || function(){};
         var dirs = [];
+        iterator(filePath, dirs, function(err, data){
+            if(err) callback(err);
+            for(var i = 0, el ; el = dirs[i++];){
+                fs.rmdirSync(el);//delete all collection dirs
+            }
+            callback(data);
+            console.log(data + 'success');
+        });
 
-        try{
-            iterator(path, dirs, function(err, data){
-                if(err) cb(err);
-                for(var i = 0, el ; el = dirs[i++];){
-                    fs.rmdirSync(el);//delete all collection dirs
-                }
-                cb(data);
-            });
-
-        }catch(e){
-            e.code === "ENOENT" ? cb() : cb(e);
-        }
-    };
-    return setInterval(ex(filePath, callback), timeInterval*1000);
+    }, interval);
 };
 
 
